5477e910282423728016f01fd7f60e48300043d3
ebonutto <elio.bonutto@gmail.com>
2026-09-18 16:22:52 +0200

    ok

--- src/formats/elf/64/segment.c
+++ src/formats/elf/64/segment.c
@@ -7,9 +7,13 @@ void compress_segment(t_woody_ctx *ctx, uint64_t offset, uint64_t size)
 	(void)ctx;
 	(void)offset;
 	(void)size;
+	// if (ctx->compression == RLE)
+	// 	rle_compress(ctx->map + offset, );
 }
 
 void encrypt_segment(t_woody_ctx *ctx, uint64_t offset, uint64_t size)
 {
-	xor(ctx->map + offset, size, (unsigned char *)ctx->key, ctx->key_size);
+	if (ctx->cipher == XOR)
+		xor(ctx->map + offset, size, (unsigned char *)ctx->key,
+	            ctx->key_size);
 }
